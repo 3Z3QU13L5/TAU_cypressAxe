06cade79f7380ea69925b9eb3ae2ab8511855109
Miguel Solano <mesolano@intellisys.com.do>
2024-02-07 10:30:48 -0400

    Testing Specific Elements with Cypress Axe

--- cypress/integration/todo-app.spec.js
+++ cypress/integration/todo-app.spec.js
@@ -1,9 +1,20 @@
 /// <reference types="cypress"/>
  
 describe('Todo application', () => {
+    beforeEach(() => {
+        cy.visit('https://todomvc.com/examples/react/dist/');
+        cy.injectAxe();
+    })
+    
     it('should log any accessibility failures', () => {
-      cy.visit('https://todomvc.com/examples/react/dist/');
-      cy.injectAxe();
       cy.checkA11y();
     });
+
+    it('should exclude specific elements on the page', () => {
+        cy.checkA11y({ exclude: ['.info'] });
+      });
+      
+      it('should only test specific element on the page', () => {
+        cy.checkA11y('.info');
+      });
    });
